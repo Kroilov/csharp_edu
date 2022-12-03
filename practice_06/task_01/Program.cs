af586af97cd248ca9f4044b0948a3302b02122a1
kroilov <popsakiller@gmail.com>
2022-12-03 19:11:50 +0300

    practice 06

---
+++ practice_06/task_01/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+using static System.Console;
+
+Write("Enter numbers separated by a space: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+
+WriteLine($"Number of positive elements: {count}");
---
+++ practice_06/task_02/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+using static System.Console;
+
+double ReadFromConsole(string input)
+{
+    Write(input);
+    return double.Parse(ReadLine());
+}
+
+double b1 = ReadFromConsole("b1 = ");
+double k1 = ReadFromConsole("k1 = ");
+double b2 = ReadFromConsole("b2 = ");
+double k2 = ReadFromConsole("k2 = ");
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+if (k1 == k2)
+    WriteLine ("No intersect");
+
+else
+    WriteLine($"Intersection point X = {x}, Y = {y}");
